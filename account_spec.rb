3659797929e80093527fbe4cd2e7a24b61b241fd
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-27 09:46:27 -0500

    added balance and account number

--- account_spec.rb
+++ account_spec.rb
@@ -4,7 +4,10 @@ require "rspec"
 require_relative "account"
 
 describe Account do
-  let(:account) { Account.new('1234567890') }
+  let(:account) {@account_number = '1234567890';
+                 @starting_balance = 0;
+                 Account.new(@account_number, @starting_balance) }
+
   describe "#initialize" do
     it "creates a Account object" do
       account.should be_an_instance_of Account
@@ -15,21 +18,27 @@ describe Account do
     end
   end
 
-
-
   describe "#transactions" do
+    it "should be an array" do
+      account.transactions.class.should eq Array
+    end
 
-    it do
-       a = Account.new('1234567890');
-       a.class == Array
-     end
+    it "should have a balance of the starting balance" do
+      account.transactions.reduce(:+) == @starting_balance
+    end
+
   end
 
   describe "#balance" do
-
+    it "sum of transactions should equal the account balance" do
+      account.transactions.reduce(:+) == account.balance
+    end
   end
 
   describe "#account_number" do
+    it "should only display last four numbers of account" do
+      last_four = /([*]*)(\d{4})/.match(@account_number)
+    end
 
   end
 
